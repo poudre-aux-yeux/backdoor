db701a24f4384c6c20be37688146a90dfbc4c020
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-03-16 12:14:33 +0100

    Added the Server class to handle starts and stops

---
+++ src/classes/Server.class.php
@@ -0,0 +1,27 @@
+<?php
+class Server extends Daemon {
+	public function __construct() {
+		// Ici on souhaite gérer les signaux SIGUSR1 et SIGUSR2 en plus
+		parent::__construct ( "server", array (
+				SIGUSR1,
+				SIGUSR2 
+		) );
+		// Démarrage du démon
+		parent::start ();
+	}
+	public function run() {
+		// Le code qui s'exécute infiniment
+		echo "On tourne !\n";
+		sleep ( 5 );
+	}
+	public function onStart() {
+		echo "Démarrage du processus avec le pid " . getmypid () . "\n";
+	}
+	public function onStop() {
+		echo "Arrêt du processus avec le pid " . getmypid () . "\n";
+	}
+	public function handleOtherSignals($signal) {
+		echo "Signal non géré par la classe Daemon : " . $signal . "\n";
+	}
+}
+?>
